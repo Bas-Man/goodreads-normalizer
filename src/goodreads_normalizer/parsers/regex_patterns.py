db9cc93ede3a8fc160f379f3b050d3aa0926f7dc
Adam.S <Bas-Man@users.noreply.github.com>
2026-06-24 10:49:50 +0900

    style(parser): Format regex strings
    #<type>(<scope>): <description> (max 50 characters)
    # Types: feat, fix, chore, refactor, docs, styles,
    # test, pref, ci, build, revert
    
    #Body:
    #<insert detailed description of changes made in the commit> (wrap at 72 characters)
    
    #Footer:
    #<insert any additional information, such as references or issue numbers>

--- src/goodreads_normalizer/parsers/regex_patterns.py
+++ src/goodreads_normalizer/parsers/regex_patterns.py
@@ -7,17 +7,21 @@ string that goodreads_export.csv contains.
 import re
 
 """ This rule handles """
-RULE_1 = re.compile(r"^(?P<Title>[\w\s'.,-]+?)(?:$|(?P<HasSeries>\s\()"
-                    r"(?(HasSeries)(?:(?P<SN1>[\w\s'&:-]+?)"
-                    r"(?:(?:,?\s#|,?\sBook\s)(?P<N1>\d+(?:-\d)?"
-                    r"(?:\.\d)?)?(?:(,\s(?P<N2>\d+(?:\.\d)?))?,\s"
-                    r"(?P<N3>\d+(?:\.\d)?))?)?[)])))"
-                    , flags=re.MULTILINE | re.IGNORECASE)
+RULE_1 = re.compile(
+    r"^(?P<Title>[\w\s'.,-]+?)(?:$|(?P<HasSeries>\s\()"
+    r"(?(HasSeries)(?:(?P<SN1>[\w\s'&:-]+?)"
+    r"(?:(?:,?\s#|,?\sBook\s)(?P<N1>\d+(?:-\d)?"
+    r"(?:\.\d)?)?(?:(,\s(?P<N2>\d+(?:\.\d)?))?,\s"
+    r"(?P<N3>\d+(?:\.\d)?))?)?[)])))",
+    flags=re.MULTILINE | re.IGNORECASE,
+)
 
-RULE_2 = re.compile(r"^(?=.*book)(?P<Title>[\w\s'-]+.*)"
-                    r"(?::\s|\s[(]+)(?P<SN1>[\s\w]+)(,\s|\s)book\s"
-                    r"(?P<N1>[\w.]+)\)?$",
-                    flags=re.MULTILINE | re.IGNORECASE)
+RULE_2 = re.compile(
+    r"^(?=.*book)(?P<Title>[\w\s'-]+.*)"
+    r"(?::\s|\s[(]+)(?P<SN1>[\s\w]+)(,\s|\s)book\s"
+    r"(?P<N1>[\w.]+)\)?$",
+    flags=re.MULTILINE | re.IGNORECASE,
+)
 
 RULE_3 = re.compile(
     r"^(?P<Title>[\w\s'.,:#-]+?)(?:$|(?P<HasSeries>\s+\()"
@@ -29,14 +33,33 @@ RULE_3 = re.compile(
 )
 
 # Crossover books like Will Trent/Jack Reacher/Forgotten realm part of multiple series
-RULE_4 = re.compile(r"(?P<Title>.*)\s\((?:(?P<ForgottenRealms>Forgotten\sRealms):\s)?"
-                    r"(?P<SN1>.*), #(?P<SN1N>\d(?:\.\d)?);\s(?P<SN2>.*)"
-                    r",\s#(?P<SN2N>\d+(?:\.\d+)?)\)",
-                    flags=re.MULTILINE | re.IGNORECASE)
-
-RULE_Z = re.compile(
-    r"^(?P<Title>[\w\s'.,:-]+)(?P<HasSeries>\s\()(?(HasSeries)((?P<SeriesName>[\w\s'&:0-9-]+)(,?\s#(?P<SeriesNumber1>\d+(-\d)?(?:\.\d)?)?(,\s(?P<SeriesNumber2>\d+(?:\.\d)?),\s(?P<SeriesNumber3>\d+(?:\.\d)?))?)?[)])$|$)",
+RULE_4 = re.compile(
+    r"(?P<Title>.*)\s\((?:(?P<ForgottenRealms>Forgotten\sRealms):\s)?"
+    r"(?P<SN1>.*), #(?P<SN1N>\d(?:\.\d)?);\s(?P<SN2>.*)"
+    r",\s#(?P<SN2N>\d+(?:\.\d+)?)\)",
     flags=re.MULTILINE | re.IGNORECASE,
 )
 
-TITLE_ONLY = re.compile(r"^(?!.*[:#()])(?P<Title>.*)$", flags=re.IGNORECASE | re.MULTILINE)
+RULE_Z = re.compile(
+    r"^(?P<Title>[\w\s'.,:-]+)(?P<HasSeries>\s\()"
+    r"(?(HasSeries)((?P<SeriesName>[\w\s'&:0-9-]+)(,?\s#(?P<SeriesNumber1>\d+(-\d)?"
+    r"(?:\.\d)?)?(,\s(?P<SeriesNumber2>\d+(?:\.\d)?),\s(?P<SeriesNumber3>\d+(?:\.\d)?))?"
+    r")?[)])$|$)",
+    flags=re.MULTILINE | re.IGNORECASE,
+)
+
+TITLE_ONLY = re.compile(
+    r"^(?!.*[:#()])(?P<Title>.*)$", flags=re.IGNORECASE | re.MULTILINE
+)
+
+ADDITIONAL_AUTHOR = re.compile(
+    r"^(?P<name>.+?)(?:\s+(?:\(|-\s+)"
+    r"((?P<type>editor|translator)|(?P<narrator>narrator)"
+    r")?\)?)?$",
+    flags=re.MULTILINE | re.IGNORECASE,
+)
+
+AUTHOR_NAME = re.compile(
+    r"(?:(?P<first_name>.*?)\s+)?(?P<last_name>[\w'-]+(?:\s+Jr\b\.?)?)",
+    flags=re.IGNORECASE,
+)
