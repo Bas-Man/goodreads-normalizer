9ef3eb5c295655a2af6329645d811165fc9a816f
Adam.S <Bas-Man@users.noreply.github.com>
2026-06-25 14:32:46 +0900

    chore: Delete old test script

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("goodreads_library_export.csv")
-pd.set_option("display.max_rows", None)
-# Force Pandas to show all columns
-pd.set_option("display.max_columns", None)
-
-print(df[["Title", "Author", "Author l-f", "My Rating", "Additional Authors"]].head(10))
