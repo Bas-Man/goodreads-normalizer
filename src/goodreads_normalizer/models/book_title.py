8d12601560bcd2ed77e6b0a7332219344df3058f
Adam.S <Bas-Man@users.noreply.github.com>
2026-07-01 15:48:32 +0900

    docs(BookTitleData): Add documentation

--- src/goodreads_normalizer/models/book_title.py
+++ src/goodreads_normalizer/models/book_title.py
@@ -18,6 +18,11 @@ class Series(BaseModel):
 class BookTitleData(BaseModel):
     """
     This model stores information extract from the "Title" row in the goodreads csv.
+
+    Stores:
+        original_title: Raw title string
+        title: parsed title string
+        series: Series Object
     """
 
     original_title: str
@@ -27,16 +32,37 @@ class BookTitleData(BaseModel):
 
     @property
     def is_a_crossover(self) -> bool:
+        """
+        Check if the book belongs to more than one series. E.G: Will Trent and Jack Reacher
+        Returns:
+            bool
+        """
         return len(self.series) > 1
 
     @property
     def is_part_of_series(self) -> bool:
+        """
+        Check if the book belongs to a series and not standalone book
+        Returns:
+            bool
+
+        """
         return len(self.series) > 0
 
     @property
     def is_stand_alone(self) -> bool:
+        """
+        This is the inverse of is_part_of_series
+        Returns:
+            bool
+        """
         return len(self.series) == 0
 
     @property
     def is_collection(self) -> bool:
+        """
+        This indicates if the book is a collection or more that one book in a series
+        Returns:
+
+        """
         return len(self.series) == 1 and len(self.series[0].numbers) > 1
