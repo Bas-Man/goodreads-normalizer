c462bd070e7f04cdceecaae2b4a9ede2d3ef7a89
Adam.S <Bas-Man@users.noreply.github.com>
2026-07-20 14:11:49 +0900

    chore(formatting): import order

--- src/goodreads_normalizer/__init__.py
+++ src/goodreads_normalizer/__init__.py
@@ -1,17 +1,22 @@
 from importlib.metadata import version
 
-from .models.author import Author
-from .models.narrator import Narrator
-from .models.book import Book
 from .io.csv import (
-    load_csv as load_csv,
-    export_to_stream as export_to_stream,
     NameFormatter as NameFormatter,
 )
+from .io.csv import (
+    export_to_stream as export_to_stream,
+)
+from .io.csv import (
+    load_csv as load_csv,
+)
+from .models.author import Author
+from .models.book import Book
+from .models.narrator import Narrator
 
 __version__ = version("goodreads-normalizer")
 
 __all__ = [
+    "__version__",
     "Author",
     "Narrator",
     "Book",
